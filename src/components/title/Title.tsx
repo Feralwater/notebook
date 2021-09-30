e2fb5272a29d94a84a9c62534d03f9d38e769e14
Feralwater <lineviktoriya@gmail.com>
2021-09-30 21:57:22 +0300

    Add: styles to title input

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent} from 'react';
+import style from "./Title.module.scss"
 
 type TitlePropsType = {
     title: string
@@ -12,11 +13,15 @@ const Title: React.VFC<TitlePropsType> = ({title, setTitle}) => {
     }
 
     return (
-        <input type="text"
-               value={title}
-               onChange={onChangHandler}
-               placeholder={"Enter title"}
-        />
+        <label className={style.input}>
+            <input type="text"
+                   className={style.input__field}
+                   value={title}
+                   onChange={onChangHandler}
+                   placeholder=" "
+            />
+            <span className={style.input__label}>Title</span>
+        </label>
     );
 };
 
